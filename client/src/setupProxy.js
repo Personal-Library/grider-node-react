d6b3bb63dbaefb5b7291b3b2e9983e8adb54f154
anthony-bui <nhut.anthony.bui@gmail.com>
2021-03-16 17:34:11 -0500

    feat: Implemented Redux boilerplate

---
+++ client/src/setupProxy.js
@@ -0,0 +1,11 @@
+// Careful here, the React documentation shows const { createProxyMiddleware } but that will result in function not found
+const createProxyMiddleware = require('http-proxy-middleware');
+
+module.exports = function (app) {
+	app.use(
+		['/api', '/auth/google'],
+		createProxyMiddleware({
+			target: 'http://localhost:5000',
+		})
+	);
+};
