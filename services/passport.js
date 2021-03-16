5a507b2e20d3fa3f3873b2603bfd6a51eba06991
anthony-bui <nhut.anthony.bui@gmail.com>
2021-03-16 17:34:11 -0500

    feat: implement redux boilerplate

--- services/passport.js
+++ services/passport.js
@@ -10,10 +10,9 @@ passport.serializeUser((user, done) => {
 	done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
-	User.findById(id).then((user) => {
-		done(null, user);
-	});
+passport.deserializeUser(async (id, done) => {
+	const user = User.findById(id);
+	done(null, user);
 });
 
 passport.use(
@@ -24,17 +23,16 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 			proxy: true,
 		},
-		(accessToken, refreshToken, profile, done) => {
+		async (accessToken, refreshToken, profile, done) => {
 			// Check to see if we have a user with profile.id
-			User.findOne({ googleId: profile.id }).then((existingUser) => {
-				if (existingUser) {
-					// we already have a record with the profile.id
-					done(null, existingUser);
-				} else {
-					// make new user if user does not exist
-					new User({ googleId: profile.id }).save().then((newUser) => done(null, newUser));
-				}
-			});
+			const existingUser = await User.findOne({ googleId: profile.id });
+			if (existingUser) {
+				// we already have a record with the profile.id
+				return done(null, existingUser);
+			}
+			// make new user if user does not exist
+			const newUser = await new User({ googleId: profile.id }).save();
+			done(null, newUser);
 		}
 	)
 );
